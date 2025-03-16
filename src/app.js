c5f5cf8930a639f19b88440504d69fa4793701e2
agrawal-harsh <letmecod@gmail.com>
2025-03-16 14:44:51 +0530

    create user register functionality

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors"
+import userRoutes from "./routes/user.routes.js"
 
 const app = express();
 
@@ -14,4 +15,6 @@ app.use(express.urlencoded({extended:true,limit:"16kb"}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+app.use("/api/v1/users",userRoutes);
 export default app;
